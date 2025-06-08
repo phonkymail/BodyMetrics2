0f646aca56c955f41c7537fb97a12e0515836ac5
Phong Le-Tran <144049752+phonkymail@users.noreply.github.com>
2025-06-08 21:19:11 +0200

    Update mongo.ts

--- nuxt/bodymetrics2/server/utils/mongo.ts
+++ nuxt/bodymetrics2/server/utils/mongo.ts
@@ -2,10 +2,6 @@ import { MongoClient, Db } from "mongodb";
 
 let cachedDb: Db;
 
-/**
- * Connects to MongoDB and caches the database instance.
- * Uses environment variables MONGODB_URI and MONGODB_DB.
- */
 export async function connectToDatabase(): Promise<Db> {
   if (cachedDb) return cachedDb;
 
@@ -14,10 +10,7 @@ export async function connectToDatabase(): Promise<Db> {
 
   try {
     const client = new MongoClient(uri, {
-      tls: true,  // Use tls (or ssl: true for older MongoDB drivers)
-      // The following two options can help during testing but should NOT be used in production:
-      // tlsAllowInvalidCertificates: true,
-      // tlsAllowInvalidHostnames: true,
+      tls: true,  
     });
     console.log('Forbinder til MongoDB via URI:', uri)
     await client.connect();
