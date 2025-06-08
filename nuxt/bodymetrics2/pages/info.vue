5005b95054116c6668f51be0ad9685ec2afbb74e
Phong Le-Tran <144049752+phonkymail@users.noreply.github.com>
2025-06-08 21:14:22 +0200

    Update info.vue

--- nuxt/bodymetrics2/pages/info.vue
+++ nuxt/bodymetrics2/pages/info.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="min-h-screen bg-cover bg-center font-sans relative"
        style="background-image: url('/webpage/pics/4.jpg')">
-    <!-- Overlay -->
+
     <div class="absolute inset-0 bg-black/60"></div>
 
-    <!-- Indhold -->
     <div class="relative z-10 flex items-center justify-center h-full px-4">
       <div class="bg-amber-50 bg-opacity-90 text-black max-w-xl w-full p-6 rounded-xl shadow-lg text-center space-y-4">
 
-        <!-- Billede -->
         <img
           src="/webpage/pics/5.png"
           alt="Projektbillede"
           class="w-32 h-32 object-contain mx-auto mb-2"
         />
 
-        <!-- Info tekst -->
         <h1 class="text-2xl font-bold">ℹ️ Om BodyMetrics</h1>
 
         <p class="text-base">
@@ -26,8 +23,6 @@
 
         <p class="text-base font-semibold">DaRaPh@2025</p>
 
-
-        <!-- Tilbage knap -->
         <NuxtLink to="/">
           <button class="mt-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded-full shadow transition">
             ⬅️ Tilbage til forsiden
